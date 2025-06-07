653cb7a1c118f2f82250ab613662e566fea0edd5
Alexey <alexey.khusnullin2015@yandex.ru>
2025-06-07 17:14:32 +0400

    adding options gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,8 +4,9 @@ import { Command } from "commander";
 const program = new Command();
 
 program
-	.name("gendiff")
-	.description("Compares two configuration files and shows a difference.")
-	.version("1.0.0")
+	.name('gendiff')
+	.description('Compares two configuration files and shows a difference.')
+	.version('1.0.0')
+	.option('-f, --format [type]', 'output format')
 
 program.parse()
