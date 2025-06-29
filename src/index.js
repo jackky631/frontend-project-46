c172075011d83767c2768dbeba5f9b814e5b4340
Alexey <alexey.khusnullin2015@yandex.ru>
2025-06-29 10:15:09 +0400

    changing index.js files

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import format from './format.js'
 import { cwd } from 'node:process'
 
 const genDiff = (filepath1, filepath2) => {
-  const getFileContent1 = readFileSync(path.resolve(cwd(), filepath1))
-  const getFileContent2 = readFileSync(path.resolve(cwd(), filepath2))
+  const getFileContent1 = readFileSync(path.resolve(cwd(), '.', '__fixtures__', filepath1))
+  const getFileContent2 = readFileSync(path.resolve(cwd(), '.', '__fixtures__', filepath2))
   const parseFile1 = parse(getFileContent1)
   const parseFile2 = parse(getFileContent2)
   const comparison = dataComparison(parseFile1, parseFile2)
