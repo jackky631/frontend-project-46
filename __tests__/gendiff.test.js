0a9e6ba1de0d0c8efd11c4adaaf66def38f7b9ad
Alexey <alexey.khusnullin2015@yandex.ru>
2025-07-03 12:25:48 +0400

    add tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,10 +9,9 @@ const __dirname = path.dirname(__filename)
 const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
 const readFile = filename => fs.readFileSync(getFixturePath(filename), 'utf-8')
 
-const file1 = getFixturePath('file1.json')
-const file2 = getFixturePath('file2.json')
-const resultFile = readFile('result_test.txt')
-
-test('gendiff', () => {
-  expect(genDiff(file1, file2)).toEqual(resultFile)
+test('gendiffJSON', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile('result_test.txt'))
+})
+test('gendiffYAML', () => {
+  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(readFile('result_test.txt'))
 })
