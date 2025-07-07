c4239a60ee67045e77d6e817be771469063f5565
Alexey <alexey.khusnullin2015@yandex.ru>
2025-07-07 15:57:15 +0400

    changed files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,9 +9,15 @@ const __dirname = path.dirname(__filename)
 const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
 const readFile = filename => fs.readFileSync(getFixturePath(filename), 'utf-8')
 
-test('gendiffJSON', () => {
+test('gendiffJSON-stylish', () => {
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile('result.txt'))
 })
-test('gendiffYAML', () => {
+test('gendiffYAML-stylish', () => {
   expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(readFile('result.txt'))
+})
+test('gendiffJSON-plain', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toEqual(readFile('result-json-plain.txt'))
+})
+test('gendiffYAML-json', () => {
+  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'json')).toEqual(readFile('result-yaml-json.txt'))
 })
--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,7 +1,14 @@
+import getPlain from './plain.js'
 import getStylish from './stylish.js'
 
 export default function makeFormat(obj, format = 'stylish') {
   if (format === 'stylish') {
     return getStylish(obj)
+  }
+  if (format === 'plain') {
+    return getPlain(obj)
+  }
+  if (format === 'json') {
+    return JSON.stringify(obj)
   }
 }
